2ddb72241b753a10526a88e4b97bb3b29555f0e8
Dantealegria1 <Dante.romero18@gmail.com>
2023-11-11 23:11:25 -0600

    hola ya jala

--- Assets/Codigo/Pacman/codigo_pacman.cs
+++ Assets/Codigo/Pacman/codigo_pacman.cs
@@ -6,9 +6,11 @@ using UnityEngine.AI;
 public class codigo_pacman : MonoBehaviour
 {
 
-     public Transform objeto1; // Debes asignar el primer objeto en el Inspector
+    public Transform objeto1; // Debes asignar el primer objeto en el Inspector
     public Transform objeto2; // Debes asignar el segundo objeto en el Inspector
     [SerializeField] Transform target; // Declarar la variable target aquí para que sea accesible en el Inspector
+    [SerializeField] Transform target_libre;
+    [SerializeField] string tagComida = "comer";
     NavMeshAgent agent;
     public Vector3 inicio = new Vector3(0.33f, 2.76f, 0f);
     public Vector3[] mitadDerecha;
@@ -16,7 +18,6 @@ public class codigo_pacman : MonoBehaviour
 
     void Start()
     {
-        crearPosiciones();
         agent = GetComponent<NavMeshAgent>();
     }
 
@@ -27,7 +28,8 @@ public class codigo_pacman : MonoBehaviour
             float distancia = Vector3.Distance(objeto1.position, objeto2.position);
             if (distancia < 10.0f)
             {
-                InicializarPerseguir();
+                //InicializarPerseguir();
+                Debug.Log("hola");
             }
             else
             {
@@ -66,45 +68,50 @@ public class codigo_pacman : MonoBehaviour
             agent.updateUpAxis = false;
             if (agent.isStopped || agent.remainingDistance <= agent.stoppingDistance)
             {
-                Vector3 destino = RandomizarCoordenadas();
-                Debug.Log(destino);
-                agent.SetDestination(destino);   
+                BuscarComida();
+            }
+        }
+    }
+    void BuscarComida()
+    {
+       
+        GameObject[] objetosComida = GameObject.FindGameObjectsWithTag(tagComida);
+        if (objetosComida.Length > 0)
+        {
+            GameObject comidaMasCercana = EncontrarComidaMasCercana(objetosComida);
+            if (comidaMasCercana != null)
+            {
+                target_libre = comidaMasCercana.transform;
+                MoverHaciaObjetivo();
             }
         }
     }
 
-    Vector3 RandomizarCoordenadas()
+    GameObject EncontrarComidaMasCercana(GameObject[] objetosComida)
     {
-        int indice = Random.Range(0, 18);
-       
-        Vector3 randomCoordinates = mitadDerecha[indice];
+        GameObject comidaMasCercana = null;
+        float distanciaMasCercana = Mathf.Infinity;
+        Vector3 posicionActual = transform.position;
 
-        return randomCoordinates;
+        foreach (GameObject comida in objetosComida)
+        {
+            float distancia = Vector3.Distance(posicionActual, comida.transform.position);
+            if (distancia < distanciaMasCercana)
+            {
+                distanciaMasCercana = distancia;
+                comidaMasCercana = comida;
+            }
+        }
+
+        return comidaMasCercana;
     }
 
-    void crearPosiciones()
+    void MoverHaciaObjetivo()
     {
-        //la derecha
-        mitadDerecha = new Vector3[18];
-        mitadDerecha[0] = new Vector3(4f, 13f, 0f);
-        mitadDerecha[1] = new Vector3(26f, 12f, 0f);
-        mitadDerecha[2] = new Vector3(17f, 4f, 0f);
-        mitadDerecha[3] = new Vector3(4f, 4f, 0f);
-        mitadDerecha[4] = new Vector3(17f, -7f, 0f);
-        mitadDerecha[5] = new Vector3(4f, -6f, 0f);
-        mitadDerecha[6] = new Vector3(4f, -12f, 0f);
-        mitadDerecha[7] = new Vector3(17f, -12f, 0f);
-        mitadDerecha[8] = new Vector3(26f, -12f, 0f);
-        mitadDerecha[9] = new Vector3(-4.52f, 12.77f, 0f);
-        mitadDerecha[10] = new Vector3(-25.96f, 13.01f, 0f);
-        mitadDerecha[11] = new Vector3(-17.06f, 9.91f, 0f);
-        mitadDerecha[12] = new Vector3(-5.99f, 4.1f, 0f);
-        mitadDerecha[13] = new Vector3(-16.85f, -6.53f, 0f);
-        mitadDerecha[14] = new Vector3(-4.07f, -5.99f, 0f);
-        mitadDerecha[15] = new Vector3(-4.62f, -12.41f, 0f);
-        mitadDerecha[16] = new Vector3(-17.17f, -13.11f, 0f);
-        mitadDerecha[17] = new Vector3(-25.96f, -13.01f, 0f);
-
+        agent.SetDestination(target_libre.position);
     }
+
+
+
 
 }
