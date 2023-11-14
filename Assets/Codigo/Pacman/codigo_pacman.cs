8e480de7218506d4469b355097e04e9e820751d7
Dantealegria1 <Dante.romero18@gmail.com>
2023-11-14 11:59:09 -0600

    portalillos

--- Assets/Codigo/Pacman/codigo_pacman.cs
+++ Assets/Codigo/Pacman/codigo_pacman.cs
@@ -10,14 +10,17 @@ public class codigo_pacman : MonoBehaviour
     [SerializeField] string tagComida = "comer";
     NavMeshAgent agent;
     public List<Transform> objetivos;  // Cambiado a una lista de objetivos
-    public Vector3 borde_izquierdo = new Vector3(-27f, 0.9736717f, 0f);
-    public Vector3 borde_derecho = new Vector3(27f, 0.9736717f, 0f);
+    public List<Transform> portales;
+    public Vector3 borde_izquierdo = new Vector3(-14.62f, -3.89f, 0f);
+    public Vector3 borde_derecho = new Vector3(35.99f, -4.04f, 0f);
     public float distanciaEscape = 8f;
     GameObject[] objetosComida;
     public Stack<GameObject> pilaComidas;
     public GameObject comida1;
     public GameObject comida2;
-
+    public GameObject comida3;
+    public LayerMask capaBarrera; // Capa que representa las barreras en tu escena
+    public float distanciaRaycast = 2f;
 
     void Start()
     {
@@ -26,24 +29,39 @@ public class codigo_pacman : MonoBehaviour
         objetosComida = GameObject.FindGameObjectsWithTag(tagComida);
         pilaComidas.Push(comida1);
         pilaComidas.Push(comida2);
-
+        pilaComidas.Push(comida3);
+        Libre();
     }
+
+    private float tiempoEsperaLibre = 1f; // Ajusta el tiempo de espera según tus necesidades
 
     void Update()
     {
-        float distanciaAlObjetivo = Vector3.Distance(transform.position, GetObjetivoMasCercano().position);
-
-        if (distanciaAlObjetivo < 10)
+        CambiarLado();
+        float distanciaAlObjetivo = Vector3.Distance(transform.position, GetObjetivoMasCercano(objetivos).position);
+        float distanciaAlPortal = Vector3.Distance(transform.position, GetObjetivoMasCercano(portales).position);
+        // Usar un umbral combinado para considerar la distancia en todas las direcciones
+        float umbralDistancia = 5f;
+        if (distanciaAlObjetivo < umbralDistancia)
         {
-      
-            Escapar();
+           if(10 < distanciaAlPortal)
+            {
+                Escapar();
+            }
+            else
+            {
+                agent.SetDestination(GetObjetivoMasCercano(portales).position);
+            }
         }
         else
         {
-
-            Libre();
+            if (!IsInvoking("Libre"))
+            {
+                Invoke("Libre", tiempoEsperaLibre);
+            }
         }
     }
+
 
     void InicializarPerseguir()
     {
@@ -65,7 +83,7 @@ public class codigo_pacman : MonoBehaviour
             agent.SetDestination(primerComida.transform.position);
             if (Vector3.Distance(transform.position, primerComida.transform.position) < 1f)
             {
-                
+
                 MoverHaciaSiguienteComida();
             }
         }
@@ -73,10 +91,10 @@ public class codigo_pacman : MonoBehaviour
 
     void MoverHaciaSiguienteComida()
     {
-        
+
         if (pilaComidas.Count > 0)
         {
-            pilaComidas.Pop(); 
+            pilaComidas.Pop();
             if (pilaComidas.Count > 0)
             {
                 GameObject siguienteComida = pilaComidas.Peek();
@@ -88,40 +106,24 @@ public class codigo_pacman : MonoBehaviour
 
     void Escapar()
     {
-        float movimiento = 5f;
         if (objetivos.Count > 0)
         {
-            Vector3 punto = GetObjetivoMasCercano().position;
-            Vector3 Nuevaposicion = new Vector3();
-            Vector3 posActual = transform.position;
-            if (punto.x > posActual.x)
-            {
-                Nuevaposicion.x = posActual.x - movimiento;
-            }
-            else
-            {
-                Nuevaposicion.x = posActual.x + movimiento; 
-            }
-            if (punto.y > posActual.y)
-            {
-                Nuevaposicion.y = posActual.y - movimiento;
-            }
-            else
-            {
-                Nuevaposicion.y = posActual.y + movimiento;
-            }
+            Transform objetivoMasCercano = GetObjetivoMasCercano(objetivos);
+            Vector3 direccionAlejarse = transform.position - objetivoMasCercano.position;
 
-            agent.SetDestination(Nuevaposicion);
+                Vector3 nuevaPosicion = transform.position + direccionAlejarse.normalized * 10f; // Ajusta el valor 5f según sea necesario
+                agent.SetDestination(nuevaPosicion);
         }
     }
 
-    Transform GetObjetivoMasCercano()
+
+    Transform GetObjetivoMasCercano(List<Transform> listaDeObjetivos)
     {
         Transform objetivoMasCercano = null;
         float distanciaMasCercana = Mathf.Infinity;
         Vector3 posicionActual = transform.position;
 
-        foreach (Transform objetivo in objetivos)
+        foreach (Transform objetivo in listaDeObjetivos)
         {
             float distancia = Vector3.Distance(posicionActual, objetivo.position);
             if (distancia < distanciaMasCercana)
@@ -137,19 +139,15 @@ public class codigo_pacman : MonoBehaviour
     void CambiarLado()
     {
         Vector3 actual = transform.position;
-
-        if (actual.y > 0 && actual.y < 1)
+        Vector3 Portalito = GetObjetivoMasCercano(portales).position;
+        float Distancia = Vector3.Distance(actual, Portalito);
+        if(Distancia<2f)
         {
-            if (actual.x == 26)
-            {
-                transform.position = borde_izquierdo;
-            }
-            else if (actual.x == -26)
-            {
-                transform.position = borde_derecho;
-            }
+            //Vector3 NuevaPosicion = portales[randomInt].position;
+            Vector3 NuevaPosicion = new Vector3(0f, 0f, 0f);
+            transform.position = NuevaPosicion;
+            Debug.Log(transform.position);
         }
     }
 
-  
 }
